8b7c9c5de2427e6748ef9e07809dfc7934dc40a3
Captain Philipp <ph-s@mail.ru>
2020-07-24 13:29:53 +0300

    Fix: from instance to class

--- lib/abstract/saga/step.rb
+++ lib/abstract/saga/step.rb
@@ -5,8 +5,21 @@ module Abstract
 
       include Shoryuken::Worker
 
-      def self.call(dto)
-        AwsSns::Sender.call(topic_key: topic_key, message: JSON[dto.to_h])
+      class << self
+        def call(dto)
+          AwsSns::Sender.call(topic_key: topic_key, message: JSON[dto.to_h])
+        end
+
+        # ! лучше сделать метамагией, так как Сага этого Шага известна из неймспейса
+        # @ abstract
+        def dto_class
+          raise NotImplementedError
+        end
+
+        # ! лучше сделать метамагией, так как ключ можно сделать из имени класса
+        def topic_key
+          raise NotImplementedError
+        end
       end
 
       # * метод для `Shoryuken::Worker`
@@ -14,17 +27,6 @@ module Abstract
         data = JSON[body]
         dto  = dto_class.wrap(data)
         call(dto)
-      end
-
-      # ! лучше сделать метамагией, так как Сага этого Шага известна из неймспейса
-      # @ abstract
-      def dto_class
-        raise NotImplementedError
-      end
-
-      # ! лучше сделать метамагией, так как ключ можно сделать из имени класса
-      def topic_key
-        raise NotImplementedError
       end
     end
   end
